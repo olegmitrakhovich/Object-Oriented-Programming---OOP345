79f42e59ba27469d7b51e2b02f292562d656f5bc
olegmitrakhovich <olegmit9@hotmail.com>
2016-12-26 15:41:40 -0500

    Add files via upload

---
+++ W7/Sale.cpp
@@ -0,0 +1,26 @@
+#include "Sale.h"
+
+
+using namespace w7;
+
+Sale::Sale(const char* ptr) {
+	std::ifstream file(ptr);
+	while (file.good()) {
+		iProduct* id = readProduct(file);
+		products.push_back(id);
+	}
+
+}
+
+void Sale::display(std::ostream& os) {
+	float total = 0;
+
+	os << "Product No     Cost Taxable\n";
+	os << std::fixed << std::setprecision(2);
+
+	for (auto p : products) {
+		os << *p;
+			total += p->getCharge();
+	}
+	os << " TOTAL" << std::setw(10) << std::right << total << '\n';
+}
