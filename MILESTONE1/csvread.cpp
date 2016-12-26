03f99ee328948dbc883a250bb666275ec0250092
olegmitrakhovich <olegmit9@hotmail.com>
2016-12-26 15:55:18 -0500

    Add files via upload

---
+++ MILESTONE1/csvread.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+// put '|' for pipe symbol
+
+using namespace std;
+
+void Trim(string& f) {
+	while (!f.empty() && f[0] == ' ') { //delete leading space
+		f.erase(0, 1);
+	}
+
+	while (!f.empty() && f[f.size() - 1] == ' ') // delete trailing spaces
+		f.erase(f.size() - 1, 1);
+}
+
+void csvPrint(vector < vector<string> > csvData) {
+	cout << "number of lines = " << csvData.size() << "\n";
+	
+	// print using indexes
+	for (size_t row = 0; row < csvData.size(); row++) {
+		cout << "row =" << row << " ";
+		for (size_t column = 0; column < csvData[row].size(); column++) {
+			cout << "'" << csvData[row][column] << "' ";
+			//cout << "\n";
+		}
+		cout << "\n";
+	}
+
+	// print using iterators
+	for (auto rowIt = csvData.begin(); rowIt != csvData.end(); rowIt++) {
+		for (auto columnIt = rowIt->begin(); columnIt != rowIt->end(); columnIt++) {
+			cout << "'" << *columnIt << "\n";
+		}
+		cout << "\n";
+	}
+
+	// print using range-based for    //ON THE EXAM
+	for (auto row : csvData) {
+		for (auto column : row) {
+			cout << "'" << column << "'";
+			//cout << "\n";
+		}
+		cout << "\n";
+	}
+}
+
+void csvReader(string& filename, char delim, vector<vector<string> > csvData) {
+	fstream is(filename, ios::in);
+	if (!is.is_open())
+		throw string("cannot open file") + filename;
+
+	string line;
+	while (getline(is, line)) {
+		auto cr = line.find('\r');
+		if (cr != string::npos)
+			line.erase(cr);
+		cout << "line ==>" << line << "<==\n";
+		vector <string> fields;
+		string field;
+		size_t i = 0;
+		while (i < line.size()) {
+			if (line[i] != delim) {
+				field += line[i];
+			}
+			else { // found delimeter
+				Trim(field);
+				fields.push_back(move(field)); // add field to our lost of fields
+				field.clear();
+			}
+			i++;
+		} // end of processing line
+		if (!field.empty()) {
+			Trim(field);
+			fields.push_back(move(field));
+			//add line fields to csvData
+			csvData.push_back(move(fields));
+			fields.clear();
+		}
+	}
+
+	is.close();
+}
+
+
+int main(int argc, char* argv[]) {
+	try {
+		if (argc != 3)
+			throw string("usage:") + argv[0] + "file.csv delimeter-char";
+		string filename = argv[1]; //1st arg
+		char delim = argv[2][0]; //1st char of second arg
+
+		vector< vector <string> > csvData;
+		csvReader(filename, delim, csvData);
+
+		csvPrint(csvData);
+	}
+	catch (string& e) {
+		cerr << e << "\n";
+	}
+}
